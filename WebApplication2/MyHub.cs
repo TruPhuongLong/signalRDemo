d6cd0507f188b40a64aeb01470ca6249e13bdb54
TruPhuongLong <longbaloca@gmail.com>
2018-06-12 16:47:39 +0700

    complete UI demo all action for signalR

--- WebApplication2/MyHub.cs
+++ WebApplication2/MyHub.cs
@@ -12,39 +12,99 @@ namespace WebApplication2
     public class MyHub : Hub
     {
         public static int count = 0;
+        static readonly HashSet<string> connectionIds = new HashSet<string>();
 
-        public void listGroup()
-        {
-            Clients.All.listGroup(Groups);
-        }
-
+        // subcribe in to group:
         public void subcribe(string group)
         {
+            // for message group:
             Groups.Add(Context.ConnectionId, group);
-            listGroup();
+
+            // for send all but this:
+            var connectionId = Context.ConnectionId;
+            connectionIds.UnionWith(
+            new[] { connectionId });
+            Clients.All.connections(connectionIds);
+            Clients.All.listId(connectionIds);
         }
-        public void message(string mes, string group)
+
+        // leave group:
+        public void unSubcribe(string group)
         {
-            var _mes = string.Format("Hello client with id: {0}, this time is: {1:F}, you are client no: {2}", Context.ConnectionId, DateTime.Now, ++count);
+            Groups.Remove(Context.ConnectionId, group);
+        }
 
-            // send all client:
-            //Clients.All.message(_mes);
+        // send all client:
+        public void messageAll(string mes)
+        {
+            var _mes = string.Format("Hello client with id: {0}, this time is: {1:F}, mes: {2}", Context.ConnectionId, DateTime.Now, mes);
+            Clients.All.message(_mes);
+        }
 
-            // send to client just come:
-            //Clients.Caller.message(_mes);
+        // send to client just come:
+        public void messageCaller(string mes)
+        {
+            var _mes = string.Format("Hello client with id: {0}, this time is: {1:F}, mes: {2}", Context.ConnectionId, DateTime.Now, mes);
+            Clients.Caller.message(_mes);
+        }
 
-            // send to group:
+
+        // send to group:
+        public void messageGroup(string mes, string group)
+        {
+            var _mes = string.Format("Hello client with id: {0}, this time is: {1:F}, mes: {2}", Context.ConnectionId, DateTime.Now, mes);
             Clients.Group(group).message(mes);
+        }
+
+        // send to other except the sender: any group can be get this mes.
+        public void messageOthers(string mes)
+        {
+            var _mes = string.Format("Hello client with id: {0}, this time is: {1:F}, mes: {2}", Context.ConnectionId, DateTime.Now, mes);
+            Clients.Others.message(mes);
+        }
+
+        // send to All except caller:
+        public void messageAllExcept(string mes, string excludeConnectionId)
+        {
+            var _mes = string.Format("Hello client with id: {0}, this time is: {1:F}, mes: {2}", Context.ConnectionId, DateTime.Now, mes);
+            var allExcept = Clients.AllExcept(excludeConnectionId);
+            allExcept.message(_mes);
         }
     }
 }
 
 /*
- Clients.
-    All
-    Caller
-    Group(groupName)
+ require for SignalR:
+ Microsoft.AspNet.SignalR library
+ +file Startup: 
+    public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
++ create hub class implement Hub
 
-Groups.
-    Add(id, GroupName)
+     */
+
+/*
++require for other app c#:
+Microsoft.AspNet.SignalR.Client library:
+var url = "http://localhost:60579";
+            var connection = new HubConnection(url);
+            var hub = connection.CreateHubProxy("echo1");
+            connection.Start().Wait();
+            hub.Invoke("HelloServer ", "Hello SignalR!");
+            Console.ReadLine();
+     */
+
+/*
+Clients
+    .All
+    .Caller
+    .Group(groupName)
+    .Others
+
+Groups
+    .Add(id, GroupName)
+    .Remove(groupName)
      */
