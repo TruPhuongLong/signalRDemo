851d3375ad30b21c1468abe9dad601f9c0857694
TruPhuongLong <longbaloca@gmail.com>
2018-06-12 14:26:55 +0700

    Clients. caller .All .Group(name); Group.Add(id, name)

--- WebApplication2/MyHub.cs
+++ WebApplication2/MyHub.cs
@@ -11,10 +11,40 @@ namespace WebApplication2
     [HubName("echo1")]
     public class MyHub : Hub
     {
-        public void HelloServer(string mes)
+        public static int count = 0;
+
+        public void listGroup()
         {
-            Debug.WriteLine("hub run");
-            Clients.All.Hello(mes);
+            Clients.All.listGroup(Groups);
+        }
+
+        public void subcribe(string group)
+        {
+            Groups.Add(Context.ConnectionId, group);
+            listGroup();
+        }
+        public void message(string mes, string group)
+        {
+            var _mes = string.Format("Hello client with id: {0}, this time is: {1:F}, you are client no: {2}", Context.ConnectionId, DateTime.Now, ++count);
+
+            // send all client:
+            //Clients.All.message(_mes);
+
+            // send to client just come:
+            //Clients.Caller.message(_mes);
+
+            // send to group:
+            Clients.Group(group).message(mes);
         }
     }
 }
+
+/*
+ Clients.
+    All
+    Caller
+    Group(groupName)
+
+Groups.
+    Add(id, GroupName)
+     */
