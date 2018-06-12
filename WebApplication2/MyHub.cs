6afc7c8adeb90f17b03bca0c020801fd89a3dde5
TruPhuongLong <longbaloca@gmail.com>
2018-06-12 11:20:28 +0700

    demosignalR

---
+++ WebApplication2/MyHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebApplication2
+{
+    [HubName("echo1")]
+    public class MyHub : Hub
+    {
+        public void HelloServer(string mes)
+        {
+            Debug.WriteLine("hub run");
+            Clients.All.Hello(mes);
+        }
+    }
+}
