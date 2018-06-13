50ed8422f8cfec0235a45fbcf520a015cb5c81ea
TruPhuongLong <longbaloca@gmail.com>
2018-06-13 14:57:36 +0700

    add PersistentConnection. it not use dynamic

--- WebApplication2/MyHub.cs
+++ WebApplication2/MyHub.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using Newtonsoft.Json;
 
 namespace WebApplication2
 {
+
+    public class EchoConnection : PersistentConnection
+    {
+        protected override Task OnConnected(IRequest request,string connectionId)
+        {
+            return Connection.Send(connectionId, "Welcome!");
+        }
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            //return Connection.Send(connectionId, data);
+
+            var payload = JsonConvert.DeserializeAnonymousType(data, new {content = "", from = "" });
+            var body = string.Format("{0} said: {1}",payload.from, payload.content);
+            return Connection.Broadcast(body);
+        }
+    }
+
+
+
     [HubName("echo1")]
     public class MyHub : Hub
     {
--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -13,6 +13,7 @@ namespace WebApplication2
         {
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
             app.MapSignalR();
+            app.MapSignalR<EchoConnection>("/echo");
         }
     }
 }
